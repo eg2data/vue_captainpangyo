950c72771ba1b0c9d9c8f7b627f6769b139300e3
eg2data <eg2data@gmail.com>
2021-11-22 15:19:24 +0900

    22nd November, <vue.js basic lecture> finished.

---
+++ vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <form v-on:submit.prevent="submitEvent">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="usernames"/>
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="password" id="password" v-model="passwords"/>
+    </div>
+    <button type="submit">send</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitEvent: function() {
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password,
+      }
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
